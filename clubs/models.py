e5f816dc51f982d7c2d9fe84dbe2c75c6d850c76
ragarsa <rarosagar.igf@gmail.com>
2020-06-28 15:16:34 -0500

    Término de la app club

--- clubs/models.py
+++ clubs/models.py
@@ -13,6 +13,6 @@ class Clubs(models.Model):
     updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return {self.name} + {self.championship}  
+        return self.name   
 
 
--- clubs/views.py
+++ clubs/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django import views
+from .models import Clubs
+from django.http import HttpResponse
 
 # Create your views here.
 def GetClubs(request):
     clubs = Clubs.objects.all()
-    return render (request)
+    template_clubs = 'clubs/list.html'
+    context = {
+        'clubs':clubs
+
+    }
+    return render(request, template_clubs, context)
+
+
+def GetClub(request, id):
+    club = Clubs.objects.get(pk=id)
+    template_club = "clubs/detail.html"
+    context = {
+        'club':club
+    }
+    return render(request, template_club, context)
+    
+
+
+
